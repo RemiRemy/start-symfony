ec1b256c56f31e9685e72d9afed8471f393acaa8
R2 <meltingpot54@gmail.com>
2021-10-14 17:04:36 +0200

    suppresion en cours

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -18,8 +18,17 @@ class PanierController extends AbstractController
         $panier = $session->get('panier', []);
 
         $detailPanier = [];
+        $total = 0;
+        $nombreProduit = 0;
 
         foreach ($panier as $idProduit => $quantite) {
+
+            $nombreProduit += $quantite;
+
+            $produit = $repo->find($idProduit);
+
+            $total += $produit->getPrix() * $quantite;
+
             $detailPanier[] = [
                 'produit' => $repo->find($idProduit),
                 'quantite' => $quantite,
@@ -28,14 +37,19 @@ class PanierController extends AbstractController
 
         return $this->render('panier/index.html.twig', [
             'detailPanier' => $detailPanier,
+            'total' => $total,
+            'nombreProduit' => $nombreProduit
         ]);
     }
 
     #[Route('/ajout-panier/{id}', name: 'ajout_panier')]
     public function ajoutPanier($id, Request $request): Response
     {
+
+        // récupère la session ( pour récupérer le panier)
         $session = $request->getSession();
 
+        // On récupère le panier et si il n'éxiste pas: un tableau vide
         $panier = $session->get('panier', []);
 
         // si il y a déjà cet article dans le panier, on augmente de 1 la quantité 
@@ -46,8 +60,15 @@ class PanierController extends AbstractController
             $panier[$id] = 1;
         }
 
+        // On obtient par exemple un tableau de ce genre [42=>5 , 19=>3 , 33=>1]
+        // (ex : l'article avec l'id 42 à une quantité de 5 dans le panier )
+
+        // On sevegarde le panier dans la session 
         $session->set('panier', $panier);
 
+        // dd($panier);
+
+        // redirection vers la page panier 
         return $this->redirect('/panier');
     }
 }
